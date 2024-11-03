61b721bbce5c7e2232e7f2da43901f1da5cae614
Anthony C. Jones <randomgeekdom@gmail.com>
2024-11-02 19:06:35 -0500

    Initial commit

---
+++ TerraTome/TerraTome/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TerraTome.Views;
+
+public partial class MainView : UserControl
+{
+	public MainView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TerraTome/TerraTome/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TerraTome.Views;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+}
